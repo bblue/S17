2738c43038e8c2df38c80acd31ffdd3624843a73
aleksander.lanes <aleksander.lanes@gmail.com>
2021-02-01 17:44:10 +0100

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r+") as fh:
 setup(
         name='S17',
         package_dir={'': 'src'},
-        version='1.0.0',
+        version='1.0.2',
         long_description=long_description,
         long_description_content_type="text/markdown",
         description='The S17 framework',
